5ee989a446593265f981ded1a4ebf55c5dea20b2
root-404 <59521514+root4041337@users.noreply.github.com>
2022-05-18 17:01:12 +0200

    Add files via upload

---
+++ FakeBitcoinWalletMiner/A/Miner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FakeBitcoinWalletMiner.A
+{
+    public class Miner
+    {
+
+        public static void _Miner()
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = 0; i < 99999; i++)
+            {
+                Console.WriteLine("[-] " + RandomString(64) + "-> 0.0000 BTC");
+            }
+
+            Thread.Sleep(12);
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine();
+            Console.WriteLine("[+] " + RandomString(64) + "-> 4.1556 BTC");
+            Console.ReadKey();
+        }
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
